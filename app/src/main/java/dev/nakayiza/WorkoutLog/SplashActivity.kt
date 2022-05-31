d096d356ba7a6f0ae7cd70300c8dac1538d652b1
julierealmids <jullietnakayizaa@gmail.com>
2022-05-31 19:29:57 +0300

    First commit

--- app/src/main/java/dev/nakayiza/WorkoutLog/SplashActivity.kt
+++ app/src/main/java/dev/nakayiza/WorkoutLog/SplashActivity.kt
@@ -1,10 +1,14 @@
 package dev.nakayiza.WorkoutLog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var intent=Intent(this,LogInActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
